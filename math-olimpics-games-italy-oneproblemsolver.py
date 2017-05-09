a8a45f426b07a866e9793b957eb89d2794e12e6a
Vincenzo Pellegrini <vincenzop3393@gmail.com>
2017-05-09 12:55:28 +0200

    Changes

--- math-olimpics-games-italy-oneproblemsolver.py
+++ math-olimpics-games-italy-oneproblemsolver.py
@@ -6,18 +6,28 @@ from time import time
 def main():
     cardset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     killers = [8, 9, 10, 8, 9, 10, 8, 9, 10, 8]
-    defeats = 0
+    defeats = [0] * 10
+
+    
 
     cases = permutations(cardset)
     total = factorial(len(cardset))
 
     for case in cases:
-        for card, killer in zip(case, killers):
+        for card, killer, count in zip(case, killers, range(0, 10)):
             if card == killer:
-                defeats += 1
+                defeats[count] += 1
                 break
 
-    print ('Ho perso ' + str(defeats) + ' volte e ho vinto ' + str(total - defeats) + ' volte su ' + str(total) + ' possibili mazzi.')
+    #print ('Ho perso ' + str(defeats) + ' volte e ho vinto ' + str(total - defeats) + ' volte su ' + str(total) + ' possibili mazzi.')
+    
+    defeats_tot = 0
+
+    for sub_defeats, count in zip(defeats, range(1, 11)):
+        print('Ho perso ' + str(sub_defeats) + ' volte per la ' + str(count) + '^ carta')
+        defeats_tot += sub_defeats
+    print('Ho perso in tutto ' + str(defeats_tot) + ' volte')
+
 
 start_time = time()
 main()
