06c1ee0c590c3eac0bf5f43092f07dfec00d12c9
Vincenzo Pellegrini <vincenzop3393@gmail.com>
2017-10-04 23:44:14 +0200

    Add files via upload

---
+++ mele.py
@@ -0,0 +1,16 @@
+pickRate = [0, 2, 3, 4, 5, 6, 7]
+                                                                                # Lunedì, martedì, mercoledì, giovedì, venerdì, sabato, domenica
+
+def picked(startDay, startPop, life):                                                     #StartDay: giorno della settimana del primo giorno di raccolta, life:durata in giorni della raccolta
+    population = startPop
+    picked = 0
+    for day in range(0 + startDay, life + startDay):
+        currentPickRate = pickRate[day % len(pickRate)]
+        if population >= currentPickRate:
+            picked += currentPickRate
+            population -= currentPickRate
+            print('test')
+        population += 1
+    return picked
+
+print(picked(5, 1, 304))
